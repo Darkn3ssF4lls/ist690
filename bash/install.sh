49c83ba5a0bd8f116e409df7b21ed341974fa2cc
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 15:25:09 -0500

    Updated to utalize virt-builder to generate a VM of CentOS 7.1 maximum size 10g and 2gb ram.

--- bash/install.sh
+++ bash/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Create a VM Style 2
+cd /var/lib/libvirt/qemu/ ; /usr/bin/virt-builder centos-7.1 --format raw --size 10G
 
-
-
+virt-install --name centos --ram 2048 --vcpus=2 --disk path=/var/lib/libvirt/qemu/centos7.1-original-vm.img  --import
 
 
 
