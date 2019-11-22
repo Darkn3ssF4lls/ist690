b05057340fead119037dd4901703272a5483b41c
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 22:23:25 -0500

    -Updated the delete-guest sxript to prompt user for info on deleting the image file. -Generated a clean-host script that will remove all the packages required for this project from the fedora host.

---
+++ bash/clean-host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#prepare fedora to manage vm's with virt
+dnf install qemu-kvm libvirt virt-install virt-manager virt-install libguestfs-tools-c -y
+
+#prepare fedora to manage vm's with OZ
+dnf install -y oz libguestfs-tools
+
+#Prepare fedora for ansible
+dnf install ansible 
+subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
--- bash/delete-guest.sh
+++ bash/delete-guest.sh
@@ -6,8 +6,12 @@ read -p 'Please enter only vm name: ' input
 virsh dumpxml --domain $input
 virsh destroy --domain $input
 virsh undefine --domain $input
+ls -l /var/lib/libvirt/qemu
+read -p 'Please select the img file to complete the deletion: ' input
 rm -rf '/var/lib/libvirt/qemu/',$input,'.img'  
+echo 'Double check your VM is gone'
 virsh list --all
+ls -l /var/lib/libvirt/qemu
 
 
 
