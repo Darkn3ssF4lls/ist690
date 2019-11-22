d888bf2d912f9b0d4a9f817da690862f0a85b9a5
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 23:22:16 -0500

    Fixed issue where qeum-kvm services did not start.

--- bash/prepare-host.sh
+++ bash/prepare-host.sh
@@ -2,6 +2,7 @@
 
 #prepare fedora to manage vm's with virt
 dnf install qemu-kvm libvirt virt-install virt-manager virt-install libguestfs-tools-c -y
+systemctl enable libvirtd && systemctl start libvirtd
 
 #prepare fedora to manage vm's with OZ
 dnf install -y oz libguestfs-tools
