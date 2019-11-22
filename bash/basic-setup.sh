24b26dec3de6ffae6dc2abe478bac7a8e8eb4360
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-22 15:32:43 -0500

    Added a basic install script for centos7

--- bash/basic-setup.sh
+++ bash/basic-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Download the Guest VM Disk Image file
-virt-install --name guest1-centos7.7 --memory 2048 --vcpus 1 --disk size=10g --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1908.iso --os-variant centos7.7 
+virt-install --name guest1-centos7.0 --memory 2048 --vcpus 1 --disk size=10g --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1908.iso --os-variant centos7.0 
 
