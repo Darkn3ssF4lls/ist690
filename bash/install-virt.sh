ebc8052dde4174f77fc00cc0f6f1a642e4f88caf
Darkn3ssF4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 23:26:00 -0500

    Updated formatting of install-virt

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -2,7 +2,6 @@
 
 #Create a Ubuntu VM with Virt Builder
 cd /var/lib/libvirt/qemu/ ; /usr/bin/virt-builder ubuntu-18.04 --format raw --size 10G &&
-
 virt-install --name ubuntu-18.04 --ram 2048 --vcpus=2 --disk path=/var/lib/libvirt/qemu/ubuntu-18.04.img  --import
 
 
