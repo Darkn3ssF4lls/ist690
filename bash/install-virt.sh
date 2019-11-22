8148aac46ab77947b8e36fbef849af14d7097d01
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-22 12:29:55 -0500

    Updated install-virt.sh remote path

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -6,5 +6,5 @@
 virt-install --network bridge:virbr0 --name testvm1 \
 --os-variant=ubuntu18.04 --ram 2048 --vcpus=2 \
 --disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=10 \
---graphics spice --location=~/Desktop/ist651/installers/ubuntu-18.04.3-desktop-amd64.iso
+--graphics spice --location=installers/ubuntu-18.04.3-desktop-amd64.iso
 
