251f2813fccf6baed9c36c27d046cd8b107a85a2
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-22 12:22:02 -0500

    Updated install-virt.sh remote path

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -6,5 +6,5 @@
 virt-install --network bridge:virbr0 --name testvm1 \
 --os-variant=ubuntu19.04 --ram 2048 --vcpus=2 \
 --disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=10 \
---graphics spice --location=http://releases.ubuntu.com/19.10/ubuntu-19.10-desktop-amd64.iso
+--graphics spice --location='http://releases.ubuntu.com/19.10/ubuntu-19.10-desktop-amd64.iso'
 
