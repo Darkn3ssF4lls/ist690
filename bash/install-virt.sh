78f5f5c4d55e2ac15f3606dce5d63dbb39a4a06f
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-22 12:01:13 -0500

    Changed the install-virt.sh to: - Install directly from ubuntu - Configured it to install a vm graphically - Configured it to be the size 10gb - Configured it to place it in the default kvm path - Configured it to be titled testvm1

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Create a Ubuntu VM with Virt Builder
-cd /var/lib/libvirt/qemu/ ; /usr/bin/virt-builder ubuntu-18.04 --format raw --size 10G &&
-virt-install --name ubuntu-18.04 --ram 2048 --vcpus=2 --disk path=/var/lib/libvirt/qemu/ubuntu-18.04.img  --import
+#Download the Guest VM Disk Image file
+#cd /var/lib/libvirt/images/ ; /usr/bin/virt-builder ubuntu19.04 --format raw --size 10G &&
 
+virt-install --network bridge:virbr0 --name testvm1 \
+--os-variant=ubuntu19.04 --ram 2048 --vcpus=2 \
+--disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=10 \
+--graphics spice --location=http://releases.ubuntu.com/19.10/ubuntu-19.10-desktop-amd64.iso
 
