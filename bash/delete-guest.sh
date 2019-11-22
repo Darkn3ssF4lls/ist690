6570cc2e5b57d07bbc8fa39c0a8ac15e56e0eb37
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 22:00:58 -0500

    Updated delete-gues with all the lines necessary to remove a vm from kvm. According to https://www.cyberciti.biz/faq/howto-linux-delete-a-running-vm-guest-on-kvm/

--- bash/delete-guest.sh
+++ bash/delete-guest.sh
@@ -4,6 +4,9 @@
 virsh list --all
 read -p 'Please enter only vm name: ' input
 virsh dumpxml --domain $input
+virsh destroy --domain $input
+virsh undefine --domain $input
+rm -rf '/var/lib/libvirt/qemu/',$input,'.img'  
 virsh list --all
 
 
