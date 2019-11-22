bb6d2f69d13827c87abe945360a89c5a5b565257
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-21 22:29:37 -0500

    Updated the install-virt script to install ubuntu-18.04 vs CentOS.

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Create a CentOS VM with Virt Builder
-cd /var/lib/libvirt/qemu/ ; /usr/bin/virt-builder centos-7.1 --format raw --size 10G -o test.img &&
+#Create a Ubuntu VM with Virt Builder
+cd /var/lib/libvirt/qemu/ ; /usr/bin/virt-builder ubuntu-18.04 --format raw --size 10G &&
 
-virt-install --name centos --ram 2048 --vcpus=2 --disk path=/var/lib/libvirt/qemu/test.img  --import
+virt-install --name ubuntu-18.04 --ram 2048 --vcpus=2 --disk path=/var/lib/libvirt/qemu/ubuntu-18.04.img  --import
 
 
