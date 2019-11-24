795af309079020d778c08e2be644926c5441bec2
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-24 16:28:02 -0500

    Updated install-template.sh to remove problem lines.

--- bash/install-template.sh
+++ bash/install-template.sh
@@ -3,7 +3,6 @@
 #Install the Guest VM from a ISO in the default directory
 virt-install --network bridge:virbr0 --name guest4-centos7.0 \
 --os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
---location=/var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso \
---disk path=/var/lib/libvirt/images/guest4-centos7.0.qcow2,format=qcow2,bus=virtio,size=10
+--location=/var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso 
 
 
