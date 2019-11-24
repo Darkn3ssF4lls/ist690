460ecc77548057b1f3af016069e6605e3be22b0a
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-24 13:00:39 -0500

    Working on disk path for install-template.sh

--- bash/install-template.sh
+++ bash/install-template.sh
@@ -4,6 +4,6 @@
 virt-install --network bridge:virbr0 --name guest4-centos7.0 \
 --os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
 --location=/var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso \
---disk path=/var/lib/libvirt/images/quest4-centos7.0.qcow2,format=qcow2,bus=virtio,size=10
+--disk path=/var/lib/libvirt/images/guest4-centos7.0.qcow2,format=qcow2,bus=virtio,size=10
 
 
