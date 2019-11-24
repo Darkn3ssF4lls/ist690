56fec344fc2f8964cefcf6e3dce32ffa9c0fa0d9
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-23 19:27:08 -0500

    Updated isntall-virt to combine the basic-isntall and its previous format. Left out the part about the disk image path.

--- bash/install-virt.sh
+++ bash/install-virt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#Download the Guest VM Disk Image file
-#cd /var/lib/libvirt/images/ ; /usr/bin/virt-builder ubuntu19.04 --format raw --size 10G &&
+#Install the Guest VM from a ISO in the default directory
+virt-install --network bridge:virbr0 --name guest3-centos7.0 \
+--os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
+--location=/var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso
 
-virt-install --network bridge:virbr0 --name testvm1 \
---os-variant=ubuntu18.04 --ram 2048 --vcpus=2 \
---disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=10 \
---graphics spice --location=installers/ubuntu-18.04.3-desktop-amd64.iso
+
+#--disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=10 \
+
 
