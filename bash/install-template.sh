8f18b753cdc3166a422ae61c2ae17895753fcbf1
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-25 16:18:38 -0500

    Fixed santax issue in install-template.sh for source iso path.

--- bash/install-template.sh
+++ bash/install-template.sh
@@ -3,6 +3,6 @@
 #Install the Guest VM from a ISO in the default directory
 virt-install --network bridge:virbr0 --name template-centos7.0 \
 --os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
---location=~ist651/installers/CentOS-7-x86_64-Everything-1908.iso 
+--location=~/ist651/installers/CentOS-7-x86_64-Everything-1908.iso 
 
 
