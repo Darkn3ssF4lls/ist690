b510250afc9ec9117c569c124d5cedd3c86cb4c0
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-25 16:05:51 -0500

    Updated the location of the CentOS iso in install-template.sh. Additionally, updated the deployed vm's name to template-centos-7.0

--- bash/install-template.sh
+++ bash/install-template.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Install the Guest VM from a ISO in the default directory
-virt-install --network bridge:virbr0 --name guest4-centos7.0 \
+virt-install --network bridge:virbr0 --name template-centos7.0 \
 --os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
---location=/var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso 
+--location=~ist651/installers/CentOS-7-x86_64-Everything-1908.iso 
 
 
