1d3059e46642cc372142dcc528245442e5f564d5
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-25 21:23:51 -0500

    Updated template sh to have more parameters to automate the install process.

--- bash/install-template.sh
+++ bash/install-template.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 #Install the Guest VM from a ISO in the default directory
-virt-install --network bridge:virbr0 --name template-centos7.0 \
---os-variant centos7.0 --memory 2048 --vcpus 1 --disk size=10 --graphics=spice \
---cdrom /installers/CentOS-7-x86_64-Everything-1908.iso --location /var/lib/libvirt/images/template-centos7.0.qcow2
+virt-install  \
+--connect qemu:///system \
+--name template-centos7.0-1 \ 
+--os-variant centos7.0 \
+--network network=default \
+--memory 1907.35 \ 
+--vcpus 1 \
+--disk pool=default, size=10, format=qcow2 \
+--graphics=spice \
+--cdrom /installers/CentOS-7-x86_64-Everything-1908.iso \
+--autostart
+
 
 
