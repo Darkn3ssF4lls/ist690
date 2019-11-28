7f1e0cf2a0cfa3992c5fda277c0e015416845aa7
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-27 21:36:33 -0500

    -Updated and tried every line of connvert-template.sh. Time to try it on the live server.

--- bash/convert-template.sh
+++ bash/convert-template.sh
@@ -7,28 +7,28 @@ echo "Welcome to the Template Generator, please follow the prompts exactly or th
 echo "The purpose of this template is to convert a vm to be a template for other vm's. Please use the clone script to perform a clone or the thin script to perform a thin clone."
 
 #
-sudo virsh list --all
+sudo virsh list --all &&
 
 #
-read -p 'Please enter only vm name: ' input
+read -p 'Please enter only vm name: ' input &&
 
 #
-sudo virsh shutdown --domain $input 
+sudo virsh shutdown --domain $input &&
 
 #
-sudo virsh autostart --disable $input
+sudo virsh autostart --disable $input &&
 
 #
-sudo virsh list --all
+sudo virsh list --all &&
 
 #
-sudo virt-sysprep -d $input --verbose
+sudo virt-sysprep -d $input --verbose &&
 
 #
-sudo virsh dumpxml --domain $input >> $input,'.xml'
+sudo virsh dumpxml --domain $input >> $input,'.xml' &&
 
 #
-sudo virsh undefine --domain $input
+sudo virsh undefine --domain $input &&
 
 #
-sudo virsh list --all
+sudo virsh list --all 
