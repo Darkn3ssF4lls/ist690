63c6db53719676e463cb0e1eb9c432a347cfe308
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-29 10:57:28 -0500

    Fixed an issue where the contents of cloing-engine.sh and install-template.sh were merged.

--- bash/cloning-engine.sh
+++ bash/cloning-engine.sh
@@ -2,3 +2,15 @@
 
 #This file prompts the user and should be designed to deploy any number of virtual machines. 
 #Generating a staic IP address for them could be in this file or I may prompt to execute another script that edits their xml files directly to make sure that their ip's arell fall inn line. 
+
+echo "This script will take a template and make a vm for it. Please follow the prompts to fill in the requirements for this cloner script."
+read -p 'Please enter the name for the new Virtual Machien: ' name
+sudo ls -la /var/lib/libvirt/images/
+read -p 'Please declare the source template, include the extention' source
+virt-install --connect qemu:///system \
+--name $name --os-variant centos7.0 \
+--network network=default --memory 2048 --vcpus 1 \
+--disk pool=default,size=10,format=qcow2 --graphics=spice \ 
+--location '/var/lib/libvirt/images/',$source \ 
+--autostart --import
+
--- bash/install-template.sh
+++ bash/install-template.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 #Install the Guest VM from a ISO in the default directory
-echo "This script will take a template and make a vm for it. Please follow the prompts to fill in the requirements for this cloner script."
-read -p 'Please enter the name for the new Virtual Machien: ' name
-sudo ls -la /var/lib/libvirt/images/
-read -p 'Please declare the source template, include the extention' source
-virt-install --connect qemu:///system \
---name $name --os-variant centos7.0 \
---network network=default --memory 2048 --vcpus 1 \
---disk pool=default,size=10,format=qcow2 --graphics=spice \ 
---location '/var/lib/libvirt/images/',$source \ 
---autostart --import
-
+virt-install --connect qemu:///system --name template1-centos7 --os-variant centos7.0 --network network=default --memory 2048 --vcpus 1 --disk pool=default,size=10,format=qcow2 --graphics=spice --location /var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso --autostart
 
 
