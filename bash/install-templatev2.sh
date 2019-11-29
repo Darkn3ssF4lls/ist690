df9aa3e43c46e5a1381f1822f74d4311ca795086
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-29 12:22:06 -0500

    Had to revert install template v2 back to local source iso

--- bash/install-templatev2.sh
+++ bash/install-templatev2.sh
@@ -7,6 +7,6 @@ echo ' '
 sudo virsh list --all
 read -p 'Please choose a vm name, it may not be listed above: ' name 
 
-sudo virt-install --connect qemu:///system --name $name --os-variant centos7.0 --network network=default --memory 2048 --vcpus 1 --disk pool=default,size=10,format=qcow2 --graphics=spice --location http://mirror.linux.duke.edu/pub/centos/7.7.1908/isos/x86_64/ --autostart
+sudo virt-install --connect qemu:///system --name $name --os-variant centos7.0 --network network=default --memory 2048 --vcpus 1 --disk pool=default,size=10,format=qcow2 --graphics=spice --location /var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso  --autostart
 
 
