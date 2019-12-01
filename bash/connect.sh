0fae68e7b20bc97534856b24fb8ca7b1ad6ed8f0
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-30 23:35:53 -0500

    Continued to update the file structure and add comments to the files .gitignore - Updated the connect.sh file with comments of its codes purpose - Renamed clean-host.sh to reset-host.sh to be more fitting its purpose - Updated the contents of reset-host.sh to properly refect the changes since the start of this project. It should properly clean the host of its qemu-kvm install as well as the iso file downloaded. If applicable should remove the current user from the libvirt group that manages the libvirt manager processes.

--- bash/connect.sh
+++ bash/connect.sh
@@ -1,5 +1,6 @@
 /!/bin/bash
 
+#During the project issues with managing the vm's became presnet. One solution that was found was to connect to the hypervisor directly. This command could be used to connect to a non local instance of qemu as well.
 #Connect to the local virt-manager
 virsh -c qemu:///system
 
