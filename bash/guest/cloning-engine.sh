4d2dcbfd14916659468c130e18d117d686f646a6
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-12-04 08:51:09 -0500

    Worked on Cloning-Engine.sh trying to get it to be user inputed

--- bash/guest/cloning-engine.sh
+++ bash/guest/cloning-engine.sh
@@ -3,14 +3,14 @@
 #This file prompts the user and should be designed to deploy any number of virtual machines. 
 #Generating a staic IP address for them could be in this file or I may prompt to execute another script that edits their xml files directly to make sure that their ip's arell fall inn line. 
 
-echo "This script will take a template and make a vm for it. Please follow the prompts to fill in the requirements for this cloner script."
-read -p 'Please enter the name for the new Virtual Machien: ' name
-sudo ls -la /var/lib/libvirt/images/
-read -p 'Please declare the source template, include the extention' source
-virt-install --connect qemu:///system \
---name $name --os-variant centos7.0 \
---network network=default --memory 2048 --vcpus 1 \
---disk pool=default,size=10,format=qcow2 --graphics=spice \ 
---location '/var/lib/libvirt/images/',$source \ 
---autostart --import
+#sudo virsh list --all &&
+#read -p 'Please choose your template from above: (only template-) ' $template
+#read -p 'Please enter one name for all your clones: ' clonename &&
+#read -p 'Please enter how many clones to make: ' clones &&
+#instance=$0 &&
 
+#while [$instance -le $clones] 
+#do 
+sudo virt-clone --original template-centos7 --name clone-centos --file /var/lib/libvirt/images/clone-centos.qcow2 --auto-clone
+#let instance++
+#done
