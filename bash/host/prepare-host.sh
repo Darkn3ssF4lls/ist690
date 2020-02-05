5374e522bec1fb2f88046aaa116326793d73e956
Dainel Lewis <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-02-05 13:34:30 -0500

    Update prepare-host.sh
    Updating to run oVirt

--- bash/host/prepare-host.sh
+++ bash/host/prepare-host.sh
@@ -2,6 +2,8 @@
 #This file is designed to download and configure the required files to turn a Fedora 30/31 desktop machine into a qemu-kvm host.
 #Download the packages as outlined in Mastering KVM Virtualization.
 sudo dnf install qemu-kvm libvirt virt-install virt-manager virt-install libguestfs-tools-c virt-viewer -y
+#Download the packages requried to activate oVirt as outlined in Mastering KVM Virtualization.
+sudo dnf install qemu-kvm-tools vdsm vdsm-cli spice server
 #Start the libvirtd process which manages the qemu-kvm instance
 sudo systemctl enable libvirtd && systemctl start libvirtd
 #Add current user to the virtualization manger groups
