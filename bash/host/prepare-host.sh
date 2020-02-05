c9e6bd7c33d65f82522b2cadd7779a33fcf3f9cf
Dainel Lewis <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-02-05 13:14:59 -0500

    Update prepare-host.sh
    Updating this repo to reflect its purpose.

--- bash/host/prepare-host.sh
+++ bash/host/prepare-host.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-#This file is designed to download and configure the required files to turn a Fedora 30 desktop machine into a qemu-kvm host.
-
+#This file is designed to download and configure the required files to turn a Fedora 30/31 desktop machine into a qemu-kvm host.
 #Download the packages as outlined in Mastering KVM Virtualization.
 sudo dnf install qemu-kvm libvirt virt-install virt-manager virt-install libguestfs-tools-c virt-viewer -y
-
 #Start the libvirtd process which manages the qemu-kvm instance
 sudo systemctl enable libvirtd && systemctl start libvirtd
-
 #Add current user to the virtualization manger groups
 sudo usermod -g $USER libvirt
 groups
-
 #Download CentOS version 7 and place it in the default pool location for qemu-kvm
-sudo wget -O /var/lib/libvirt/images/CentOS-7-x86_64-Everything-1908.iso "http://mirrors.rit.edu/centos/7.7.1908/isos/x86_64/CentOS-7-x86_64-Everything-1908.iso"
+sudo wget -O /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1908.iso "http://mirror.sjc02.svwh.net/centos/7.7.1908/isos/x86_64/CentOS-7-x86_64-Minimal-1908.iso"
