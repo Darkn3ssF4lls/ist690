5a779c90870cf453ec378b1dd02b24a504e79937
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-02-20 17:11:42 -0500

    Continue to Refine the Installer for Human Consumption

--- bash/guest/installer.sh
+++ bash/guest/installer.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-sudo virsh list --all 
+sudo virt-install --connect qemu:///system \
+--name test \
+--os-variant centos7.0 \
+--network network=default \
+--memory 2048 \
+--vcpus 1 \
+--disk pool=default,size=10,format=qcow2 \
+--graphics=spice \
+--location /var/lib/libvirt/images/centos8.iso \
+--autostart &&
 
-sudo virt-install --connect qemu:///system --name test --os-variant centos7.0 --network network=default --memory 2048 --vcpus 1 --disk pool=default,size=10,format=qcow2 --graphics=spice --location /var/lib/libvirt/images/centos8.iso --autostart
 
 
