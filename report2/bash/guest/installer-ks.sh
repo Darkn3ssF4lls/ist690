da22e3f704c5be8d38bf2cc626fb808bdb441441
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-03-13 17:48:52 -0400

    Working on kickstart issues.

--- report2/bash/guest/installer-ks.sh
+++ report2/bash/guest/installer-ks.sh
@@ -3,7 +3,5 @@ sudo virt-install --connect qemu:///system --name centos7-ks --memory 2048 \
 --disk pool=default,size=10,format=qcow2 --os-variant centos7.0 \
 --location /var/lib/libvirt/images/centos7.iso \
 --initrd-inject='/home/localadmin/ist690/report2/kickstart/ks.cfg' \
---extra-args="ks=file:/ks.cfg"
-
-
-#
+--extra-args="ks=file:/ks.cfg" \
+--autostart
