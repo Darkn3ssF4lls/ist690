20bb74e9fc0671c7487f8ddac44a04061d228e4b
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-03-16 00:24:32 -0400

    Trying out Fedora Build with Kickstart.

--- report2/bash/guest/cent-ks.sh
+++ report2/bash/guest/cent-ks.sh
@@ -2,6 +2,6 @@
 sudo virt-install --connect qemu:///system --name centos7-ks --memory 2048 \
 --disk pool=default,size=10,format=qcow2 --os-variant centos7.0 \
 --location /var/lib/libvirt/images/centos7.iso \
---initrd-inject='/home/localadmin/ist690/report2/kickstart/ks.cfg' \
---extra-args="ks=file:/ks.cfg" \
+--initrd-inject='/home/localadmin/ist690/report2/kickstart/cent-ks.cfg' \
+--extra-args="ks=file:/cent-ks.cfg" \
 --autostart
