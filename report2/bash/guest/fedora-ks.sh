6e9e168cf168487cf2f1655e38ad3db7c2fde2a8
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-03-16 00:23:49 -0400

    Trying out Fedora Build with Kickstart.

---
+++ report2/bash/guest/fedora-ks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo virt-install --connect qemu:///system --name fedora31-ks --memory 2048 \
+--disk pool=default,size=10,format=qcow2 --os-variant fedora29 \
+--location /var/lib/libvirt/images/fedora31.iso \
+--initrd-inject='/home/localadmin/ist690/report2/kickstart/ks.cfg' \
+--extra-args="ks=file:/ks.cfg" \
+--autostart
