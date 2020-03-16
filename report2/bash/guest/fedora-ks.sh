09bcaff6900a218a3a5fc4319943079e01019efc
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-03-16 01:54:43 -0400

    Got both cent and fedora up and running.

--- report2/bash/guest/fedora-ks.sh
+++ report2/bash/guest/fedora-ks.sh
@@ -2,6 +2,6 @@
 sudo virt-install --connect qemu:///system --name fedora31-ks --memory 2048 \
 --disk pool=default,size=10,format=qcow2 --os-variant fedora29 \
 --location /var/lib/libvirt/images/fedora31.iso \
---initrd-inject='/home/localadmin/ist690/report2/kickstart/ks.cfg' \
---extra-args="ks=file:/ks.cfg" \
+--initrd-inject='/home/localadmin/ist690/report2/kickstart/fedora-ks.cfg' \
+--extra-args="ks=file:/fedora-ks.cfg" \
 --autostart
