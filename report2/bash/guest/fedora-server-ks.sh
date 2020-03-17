d4ed9e774acd634a053373493e687263d811db9f
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-03-17 14:35:03 -0400

    Updated naming of both VM's for Fedora install file.

--- report2/bash/guest/fedora-server-ks.sh
+++ report2/bash/guest/fedora-server-ks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo virt-install --connect qemu:///system --name fedora31-ks --memory 2048 \
+sudo virt-install --connect qemu:///system --name fedora31-server-ks --memory 2048 \
 --disk pool=default,size=10,format=qcow2 --os-variant fedora29 \
 --location /var/lib/libvirt/images/fedora31-server.iso \
 --initrd-inject='/home/localadmin/ist690/report2/kickstart/fedora-ks.cfg' \
