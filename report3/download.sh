51716d1a70ae5bcde8bf0b8d8bdd737564dcbae0
Darkn3ssF4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-04-03 21:54:46 -0400

    Add report 3's preparation download file.

---
+++ report3/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#This code creates Ubuntu 19.0 server iso in the image directory. 
+sudo wget -O /var/lib/libvirt/images/ubuntu19-server.iso 'http://releases.ubuntu.com/19.10/ubuntu-19.10-live-server-amd64.iso'
