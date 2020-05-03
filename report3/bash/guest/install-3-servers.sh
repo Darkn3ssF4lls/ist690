5e6b360e35cf196385b09e35fa5d56f9374ab47a
Dainel Lewis <39456389+Darkn3ssF4lls@users.noreply.github.com>
2020-05-03 04:02:01 -0400

    Delete install-3-servers.sh
    updating to report 3 current status

--- report3/bash/guest/install-3-servers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo virt-install --connect qemu:///system \
---name controller \
---memory 4096 \
---cpu 2
---disk pool=vms,size=20,format=qcow2 \
---location /var/lib/libvirt/images/ubuntu-18.04.4.iso
-
